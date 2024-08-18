48bbe7d9cf8f0dd5168b0ff7e2398d1f4acebec3
gourab8389 <deyg6988@gmail.com>
2024-08-19 01:44:44 +0530

    feat: Add mobile navigation drawer
    The commit adds a mobile navigation drawer to the Navbar component. The drawer includes a heart handshake icon and a menu icon. Clicking the menu icon toggles the visibility of the drawer, which displays a list of navigation links. Clicking on a link updates the active state and navigates to the corresponding page. This change enhances the user experience by providing a responsive and accessible navigation system for mobile devices.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,23 @@
-import React, { useCallback } from 'react'
-import { search } from '../assets'
+import React, { useCallback, useState } from 'react'
+import { menu, search } from '../assets'
 import CustomButton from './CustomButton'
 import { usePrivy } from '@privy-io/react-auth'
+import { IconHeartHandshake } from '@tabler/icons-react'
+import { navLinks } from '../constants'
+import { useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
+    const navigate = useNavigate()
+    
     const {ready, authenticated, login, user, logout} = usePrivy()
+
+    const [toggleDrawer,setToggleDrawer] = useState(false)
+
+    const [isActive, setIsActive] = useState("dashboard")
+
+    console.log("user info", user)
+
 
     const handleLoginLogout = useCallback(()=>{
         if(authenticated){
@@ -43,6 +55,34 @@ const Navbar = () => {
         styles={authenticated ? "bg-[#1dc071]": "bg-[#8c6dfd]"}
         handleClick={handleLoginLogout}
         />
+      </div>
+      <div className="relative flex items-center justify-between sm:hidden">
+        <div className="flex h-[40px] cursor-pointer items-center justify-center rounded-[10px] bg-[#2c2f32]">
+            <IconHeartHandshake size={40} color='#1ec070' className='p-2'/>
+        </div>
+        <img src={menu} alt="menu" className='h-[34px] w-[34px] cursor-pointer object-contain' onClick={()=>{setToggleDrawer((prev)=> !prev)}}/>
+
+        <div className={`absolute left-0 right-0 top-[60px] px-2 z-10 bg-[#1c1c24] py-4 shadow-secondary ${!toggleDrawer ? "-translate-y-[100vh]":"translate-y-0"} transition-all duration-700`}>
+            <ul className='mb-4'>
+                {navLinks.map((link)=> (
+                    <li key={link.name} className={`flex gap-3 items-center p-4 ${isActive === link.name && "bg-[#3a3a43]"}`}
+                    onClick={()=>{
+                        setIsActive(link.name)
+                        setToggleDrawer(false)
+                        navigate(link.link)
+                    }}
+                    >
+                        <img 
+                        src={link.imageUrl} alt="nav menu" className={`h-[14px] w-[14px] ${isActive === link.name ? 'bg-[#1dc071]' : 'bg-[#808191]'}`} />
+
+                        <p className={`font-epilogue text-[14px] font-semibold ${isActive === link.name ? 'text-[#1dc071]' : 'text-[#808191]'}`}>
+        {link.name}
+      </p>
+                    </li>
+                ))}
+            </ul>
+            <div className="mx-4 flex"></div>
+        </div>
       </div>
     </div>
   )
