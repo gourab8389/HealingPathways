19492fecf72bce239b3f9408843e34b8f76d6f25
gourab8389 <deyg6988@gmail.com>
2024-08-19 22:49:39 +0530

    feat: Improve lazy loading for images in DisplayInfo component

--- src/components/homeComponents/displayInfo.jsx
+++ src/components/homeComponents/displayInfo.jsx
@@ -1,6 +1,12 @@
+import { IconCircleDashedCheck, IconHourglassHigh, IconUserScan } from '@tabler/icons-react'
 import React, { useState } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
+
 
 const DisplayInfo = () => {
+
+  const navigate = useNavigate();
+
     const [metrics, setMetrics] = useState({
         totalFolders: 0,
         aiPersonalizedTreatment: 0,
@@ -11,8 +17,54 @@ const DisplayInfo = () => {
     })
     const metricsData = [
         {
-            
-        }
+            title: "Specialist Appointments Pending",
+            subtitle: "View",
+            value: metrics.pendingScreenings,
+            icon:IconHourglassHigh,
+            onclick: () => navigate('/appointments/pending')
+        },
+        {
+            title: "Treatment Process Update",
+            subtitle: "View",
+            value: `${metrics.completedScreenings} of ${metrics.totalScreenings}`,
+            icon:IconCircleDashedCheck,
+            onclick: () => navigate('/appointments/progress')
+        },
+        {
+            title: "Total Folders",
+            subtitle: "View",
+            value: metrics.totalFolders,
+            icon:IconCircleDashedCheck,
+            onclick: () => navigate('/folders')
+        },
+        {
+            title: "Total Screenings",
+            subtitle: "View",
+            value: metrics.totalScreenings,
+            icon:IconUserScan,
+            onclick: () => navigate('/screenings')
+        },
+        {
+            title: "Completed Screenings",
+            subtitle: "View",
+            value: metrics.completedScreenings,
+            icon:IconUserScan,
+            onclick: () => navigate('/screenings/completed')
+        },
+        {
+            title: "Pending Screenings",
+            subtitle: "View",
+            value: metrics.pendingScreenings,
+            icon:IconUserScan,
+            onclick: () => navigate('/screenings/pendings')
+        },
+        {
+            title: "Overdue Screenings",
+            subtitle: "View",
+            value: metrics.overdueScreenings,
+            icon:IconUserScan,
+            onclick: () => navigate('/screenings/overdue')
+        },
     ]
   return (
     <div>
