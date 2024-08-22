d0a52c61981e9dfff6b4546c78698663eb5d3fc8
gourab8389 <deyg6988@gmail.com>
2024-08-22 22:03:03 +0530

    feat: Add Users and Records tables to schema
    This commit adds the Users and Records tables to the schema in the src/utils/schema.jsx file. The Users table includes columns for id, username, age, and createdBy. The Records table includes columns for id, userId, recordName, analysisResult, kanbanRecords, and createdBy. This change enhances the database structure by providing tables to store user and record data.

--- src/utils/schema.jsx
+++ src/utils/schema.jsx
@@ -1,2 +1,18 @@
 import { sql } from "drizzle-orm";
 import { integer, varchar, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const Users = pgTable('users',{
+    id: serial("id").primaryKey,
+    username: varchar("username").notNull(),
+    age: integer("age").notNull(),
+    createBy: varchar("created_by").notNull(),
+});
+
+export const Records = pgTable("records",{
+    id: serial("id").primaryKey,
+    userId: integer("user_id").references(()=> Users.id).notNull(),
+    recodName: varchar("record_name").notNull(),
+    analysisResult: varchar("analysis_result").notNull(),
+    kanbanRecords: varchar("kanban_records").notNull(),
+    createdBy: varchar("created_by").notNull(),
+})
