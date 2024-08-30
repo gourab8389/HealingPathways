7b050dee958c7a2a3c19cb8f296b29ff59aa4ebe
gourab8389 <deyg6988@gmail.com>
2024-08-31 00:00:08 +0530

    refactor: Improve layout and design of SingleRecordDetails page
    This commit refactors the SingleRecordDetails page component to improve its layout and design. It includes enhancements such as adding a folder icon and a clickable record name. These changes enhance the visual presentation of the record details and improve the user experience.

--- src/pages/records/single-record-details.jsx
+++ src/pages/records/single-record-details.jsx
@@ -11,46 +11,79 @@ const SingleRecordDetails = () => {
     const { state } = useLocation();
     return (
         <div className="flex flex-wrap gap-[26px]">
-            <button
-                type="button"
-                // onClick={handleOpenModal}
-                className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
-            >
-                <IconFileUpload />
-                Upload Reports
-            </button>
-
-            {/* fileUploadModal */}
-            <RecordDetailsHeader recordName={state} />
-        <div className="w-full">
-            <div className="flex flex-col">
-                <div className="-m-1.5 overflow-x-auto">
-                    <div className="inline-block min-w-full p-1.5 align-middle">
-                        <div className="overflow-hidden rounded-xl border-neutral-700 shadow-sm bg-[#13131a]">
-                            <div className="border-b border-neutral-700 px-6 py-4">
-                                    <h2 className="text-xl font-semibold text-neutral-200">
-                                        Personalized AI-Driven Treatment Plan
-                                    </h2>
-                                    <p className="text-sm text-neutral-400">
-                                        A tailored medical strategy leveraging advanced AI insights.
-                                    </p>
-                            </div>
-
-                            <div className="flex w-full flex-col px-6 py-4 text-white">
-                                    <h2 className="text-lg font-semibold text-white">
-                                        Analysis Result
-                                    </h2>
-
-                                    <div className="space-y-2">
-                                        {'rendering the result here'}
-                                    </div>
-                            </div>
-                        </div>
-                    </div>
+      <button
+        type="button"
+        // onClick={handleOpenModal}
+        className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
+      >
+        <IconFileUpload />
+        Upload Reports
+      </button>
+      {/* <FileUploadModal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onFileChange={handleFileChange}
+        onFileUpload={handleFileUpload}
+        uploading={uploading}
+        uploadSuccess={uploadSuccess}
+        filename={filename}
+      /> */}
+      <RecordDetailsHeader recordName={state.recordName} />
+      <div className="w-full">
+        <div className="flex flex-col">
+          <div className="-m-1.5 overflow-x-auto">
+            <div className="inline-block min-w-full p-1.5 align-middle">
+              <div className="overflow-hidden rounded-xl border border-gray-200 bg-white shadow-sm dark:border-neutral-700 dark:bg-[#13131a]">
+                <div className="border-b border-gray-200 px-6 py-4 dark:border-neutral-700">
+                  <h2 className="text-xl font-semibold text-gray-800 dark:text-neutral-200">
+                    Personalized AI-Driven Treatment Plan
+                  </h2>
+                  <p className="text-sm text-gray-600 dark:text-neutral-400">
+                    A tailored medical strategy leveraging advanced AI insights.
+                  </p>
                 </div>
+                <div className="flex w-full flex-col px-6 py-4 text-white">
+                  <div>
+                    <h2 className="text-lg font-semibold text-gray-800 dark:text-white">
+                      Analysis Result
+                    </h2>
+                    <div className="space-y-2">
+                      {/* <ReactMarkdown>{analysisResult}</ReactMarkdown> */}
+                    </div>
+                  </div>
+                  <div className="mt-5 grid gap-2 sm:flex">
+                    <button
+                      type="button"
+                    //   onClick={processTreatmentPlan}
+                      className="inline-flex items-center gap-x-2 rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-white dark:hover:bg-neutral-800"
+                    >
+                      View Treatment plan
+                      <IconChevronRight size={20} />
+                      {/* {processing && (
+                        <IconProgress
+                          size={10}
+                          className="mr-3 h-5 w-5 animate-spin"
+                        />
+                      )} */}
+                    </button>
+                  </div>
+                </div>
+                <div className="grid gap-3 border-t border-gray-200 px-6 py-4 md:flex md:items-center md:justify-between dark:border-neutral-700">
+                  <div>
+                    <p className="text-sm text-gray-600 dark:text-neutral-400">
+                      <span className="font-semibold text-gray-800 dark:text-neutral-200"></span>{" "}
+                    </p>
+                  </div>
+                  <div>
+                    <div className="inline-flex gap-x-2"></div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-        </div>
+      </div>
+    </div>
     )
 }
 
