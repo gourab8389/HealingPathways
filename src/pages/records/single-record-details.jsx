63b29f3a2b9fffcb09e8451fde0fb579ceca1caf
gourab8389 <deyg6988@gmail.com>
2024-08-30 23:58:45 +0530

    feat: Improve layout and design of SingleRecordDetails page
    This commit enhances the layout and design of the SingleRecordDetails page component. It includes improvements such as adding a folder icon and a clickable record name. These changes enhance the visual presentation of the record details and improve the user experience.

--- src/pages/records/single-record-details.jsx
+++ src/pages/records/single-record-details.jsx
@@ -8,20 +8,48 @@ import { useLocation } from "react-router-dom";
 
 const SingleRecordDetails = () => {
 
-    const {state} = useLocation();
+    const { state } = useLocation();
     return (
         <div className="flex flex-wrap gap-[26px]">
-            <button 
-            type="button" 
-            // onClick={handleOpenModal}
-            className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
+            <button
+                type="button"
+                // onClick={handleOpenModal}
+                className="mt-6 inline-flex items-center gap-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-800 shadow-sm hover:bg-gray-50 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-[#13131a] dark:text-white dark:hover:bg-neutral-800"
             >
-                <IconFileUpload/>
+                <IconFileUpload />
                 Upload Reports
             </button>
 
             {/* fileUploadModal */}
-            <RecordDetailsHeader recordName={state}/>
+            <RecordDetailsHeader recordName={state} />
+        <div className="w-full">
+            <div className="flex flex-col">
+                <div className="-m-1.5 overflow-x-auto">
+                    <div className="inline-block min-w-full p-1.5 align-middle">
+                        <div className="overflow-hidden rounded-xl border-neutral-700 shadow-sm bg-[#13131a]">
+                            <div className="border-b border-neutral-700 px-6 py-4">
+                                    <h2 className="text-xl font-semibold text-neutral-200">
+                                        Personalized AI-Driven Treatment Plan
+                                    </h2>
+                                    <p className="text-sm text-neutral-400">
+                                        A tailored medical strategy leveraging advanced AI insights.
+                                    </p>
+                            </div>
+
+                            <div className="flex w-full flex-col px-6 py-4 text-white">
+                                    <h2 className="text-lg font-semibold text-white">
+                                        Analysis Result
+                                    </h2>
+
+                                    <div className="space-y-2">
+                                        {'rendering the result here'}
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
