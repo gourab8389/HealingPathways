1e63c320a914763f36209beb967b3373a8f15685
gourab8389 <deyg6988@gmail.com>
2024-09-01 00:56:59 +0530

    refactor: Update database configuration to use environment variables
    This commit updates the database configuration in the `drizzle.config.js` and `dbConfig.jsx` files to use environment variables for the database URL and connection string. Instead of hardcoding the values, the `process.env.NEON_DB_URL` variable is used, allowing for greater flexibility and security. This change improves the maintainability and security of the application by separating sensitive information from the codebase.

--- src/utils/dbConfig.jsx
+++ src/utils/dbConfig.jsx
@@ -1,11 +1,7 @@
-import {neon} from "@neondatabase/serverless";
-
-import {drizzle} from "drizzle-orm/neon-http";
-
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(
-    "postgresql://duo_linga_owner:3Zz8usHLSNmQ@ep-sweet-wildflower-a5nmoq6r.us-east-2.aws.neon.tech/cancer_ai?sslmode=require"
-);
+const sql = neon(process.env.NEON_DB_URL);
 
-export const db = drizzle(sql, {schema});
+export const db = drizzle(sql, { schema });
